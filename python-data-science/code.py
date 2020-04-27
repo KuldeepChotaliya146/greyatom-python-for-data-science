e71a5ba7bddd570d2d8653fbecb05064a23b9683
KuldeepChotaliya146 <KuldeepChotaliya146@users.noreply.github.com>
2020-04-27 14:50:56 +0530

    Code file created.

---
+++ python-data-science/code.py
@@ -0,0 +1,33 @@
+# --------------
+# Code starts here
+# Create the lists 
+class_1 = ['Geoffrey Hinton','Andrew Ng','Sebastian Raschka','Yoshua Bengio']
+class_2 = ['Hilary Mason','Carla Gentry','Corinna Cortes']
+# Concatenate both the strings
+new_class = class_1 + class_2
+#print(new_class)
+# Append the list
+new_class.append('Peter Warden')
+#print(new_class)
+# Print updated list
+new_class.remove('Carla Gentry')
+#print(new_class)
+# Create the Dictionary
+courses = {'Math':65,'English':70,'History':80,'French':70,'Science':60}
+list = courses.values()
+total = sum(list)
+#print(total)
+percentage = (total/500) * 100
+#print(percentage)
+# Create the Dictionary
+mathematics = {'Geoffery Hinton':78,'Andrew Ng':95,'Sebastian Raschka':65,'Yoshua Benjio':50,'Hilary Mason':70,'Corinna Cortes':66,'Peter Warden':75} 
+topper = max(mathematics,key = mathematics.get)
+print(topper)
+first_name = topper.split()[0]
+last_name = topper.split()[1]
+full_name = last_name + " " + first_name
+certificate_name = full_name.upper()
+print(certificate_name)
+# Code ends here
+
+
